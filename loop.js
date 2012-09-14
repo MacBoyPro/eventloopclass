8bda4767a32b93225626d2c527839f0d290d0e6b
Lee Lundrigan <lee.lundrigan@gmail.com>
2012-09-14 10:22:55 -0700

    implemented nextTick

--- loop.js
+++ loop.js
@@ -23,19 +23,34 @@ var timers = [];
 var timer = 0;
 exports.in = function(sec, callback) {
   timers.push({ timeout: timer + sec, callback: callback });
-
 };
+
+var nextTicks = [];
+exports.nextTick= function(callback) {
+  nextTicks.push(callback);
+}
 
 exports.run = function() {
   // Here is the event LOOP!
   while(true) {
+    
+    //call our nextTick functions
+    for (var i = 0; i < nextTicks.length; i += 1) {
+      nextTicks[i]();
+    }
+
+    nextTicks = [];
+
+    if (Object.keys(readables).length == 0 && Object.keys(writeables).length == 0 && timers.length == 0) return;
+
     var fds = syscalls.select(Object.keys(readables), Object.keys(writeables), [], 1);
     
     timer += 1;
     for (var i = 0; i < timers.length; i += 1) {
-      if(timers[i].timeout >= timer) {
+      if(timers[i].timeout <= timer) {
         timers[i].callback();
         timers.splice(i, 1);
+        i--;
       }
     }
 
--- next_tick.js
+++ next_tick.js
@@ -1,5 +1,18 @@
-process.nextTick(function() {
-  console.log("will this run first ....");
+//process.nextTick(function() {
+  //console.log("will this run first ....");
+//});
+
+//console.log("or is this first?");
+//
+
+var loop = require('./loop');
+
+loop.nextTick(function(){
+  console.log('next tick');
 });
 
-console.log("or is this first?");
+console.log('before next tick');
+
+loop.run();
+
+
