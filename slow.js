f4af34552ab4946058130ef2ae3b8125f2b20376
Lee Lundrigan <lee.lundrigan@gmail.com>
2012-09-14 11:22:58 -0700

    fixed the slow response of the server

--- slow.js
+++ slow.js
@@ -2,17 +2,27 @@ var http = require('http');
 
 // NOTE: We're now using Node.js builtin event loop. NOT the loop we built in loop.js
 
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
   if (req.url == "/slow") {
     var objects = [];
-    
-    for (var i=0; i < 10000000; i++) {
-      objects.push(new Object()); // pretend we're computing something here
-    };
-    res.end("slow request done");
-        
+    var counter = 10000000;
+
+    function compute() {
+      objects.push(new Object);
+      if(counter > 0) {
+        process.nextTick(compute);
+        counter--;
+      }
+
+      if (counter == 0) {
+        res.end("slow request done, counter=" + counter + " objects=" + objects.length);
+      }
+    }
+
+    compute();
   } else {
     res.end("fast request done");
     
